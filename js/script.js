7dc74e236647a4b50ccd815aad1b0b7c00d7f7b2
Giulio <giulio.tavoni@gmail.com>
2021-01-26 17:14:17 +0100

    Esercizio basic

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Ricreate lo slider visto in jQuery con vue. Personalizzatelo come meglio credete.
+
+var app = new Vue({
+  el: '#container',
+  data: {
+    counter: 0,
+    imgsSrc: [
+      'img/aladdin.jpg',
+      'img/dumbo.jpg',
+      'img/il-re-leone.jpg',
+      'img/libro-giungla.jpg',
+      'img/peter-pan.jpg',
+    ]
+  },
+    methods: {
+      nextPhoto() {
+        (this.counter == this.imgsSrc.length - 1) ? this.counter = 0 : this.counter++;
+        console.log(this.counter);
+      },
+      prevPhoto() {
+        (this.counter == 0) ? this.counter = this.imgsSrc.length - 1 : this.counter--;
+        console.log(this.counter);
+      },
+      changePhoto(index) {
+        this.counter = index;
+      }
+    }
+})
